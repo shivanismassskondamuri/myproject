43225183820047870093f335a887bacafc6366a3
vijayalakshmi <karri.vijayalakshmi2101@gmail.com>
2019-02-16 15:35:19 +0530

    'hihello'

---
+++ sw.js
@@ -0,0 +1,32 @@
+var cache1="c2";
+this.addEventListener("install",function(event){
+	event.waitUntil(
+		caches.open(cache1).then(cache=>{
+			cache.addAll([
+				// "style.css",
+				"form.css",
+				"resume.css",
+				"resumeicon.png"
+				])
+		})
+
+
+		);
+});
+
+this.addEventListener('fetch',function(event){
+	event.respondWith(
+		caches.open(cache1).then(function(cache){
+			return cache.match(event.request).then(function(res){
+				return res || fetch(event.request).then(function(reqres){
+					cache.put(event.request,reqres.clone());
+				})
+			})
+
+		})
+
+
+
+		)
+
+})
